dd49e7875b5260b58eb07b81e2522c079f408e28
Reza <rzgholizadeh@gmail.com>
2020-03-27 19:56:45 -0600

    feat: Add the code to ensure the file given exists

--- index.js
+++ index.js
@@ -2,17 +2,27 @@
 const debounce = require("lodash.debounce");
 const chokidar = require("chokidar");
 const program = require("caporal");
+const fs = require("fs");
 
-const start = debounce(() => {
-	console.log("STARTING USERS PROGRAM");
-}, 200);
+program
+	.version("0.0.1")
+	.argument("[filename]", "Name of a file to execute")
+	.action(async ({ filename }) => {
+		const name = filename || "index.js";
+		try {
+			await fs.promises.access(name);
+		} catch (err) {
+			throw new Error(`Could not find the file ${name}`);
+		}
+		const start = debounce(() => {
+			console.log("STARTING USERS PROGRAM");
+		}, 200);
 
-chokidar
-	.watch(".")
-	.on("add", start)
-	.on("change", () => {
-		console.log("FILE CHANGED");
-	})
-	.on("unlink", () => {
-		console.log("FILE UNLINKED");
+		chokidar
+			.watch(".")
+			.on("add", start)
+			.on("change", start)
+			.on("unlink", start);
 	});
+
+program.parse(process.argv);
